e27a16eb483f8f06a3e4119516930744fe068c88
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-05 13:57:52 +0100

    created a post create mock for get and  post HTTP methods

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Log;
 class PostController extends Controller
 {
     
     function show($post) {
         return view("posts.post",$post);
+    }
+    function create_form(){
+        return view("/posts/create");
+    }
+    function create(Request $request){
+        $author= $request->input("author");
+        // Log::info($author);
+        return redirect("/");
+
     }
 }
