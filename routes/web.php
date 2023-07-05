7dd0c2fc48b21ac65343e5656bbdfd58ff4131c1
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-05 13:59:06 +0100

    created routing for posts/create POST&GET methods

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts/create',PostController::class . "@create_form"
+// function(){
+    // return view("posts.create");
+// }
+);
+Route::post("/posts/create", PostController::class . "@create");
