6a33d0f9a904dcf9d2024f17ec659f7456154757
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-08 15:54:47 +0100

    add auth middleware only to /posts/create route

--- routes/posts.php
+++ routes/posts.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware("auth")->group(function(){
 Route::get("/posts/create",PostController::class . "@create_form");
+});
 Route::post("/posts/create",PostController::class . "@create");
+
 Route::get("posts",PostController::class . "@show");
