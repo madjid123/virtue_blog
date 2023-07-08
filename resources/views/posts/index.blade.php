523a5550567dda9e4e5ca9b8c4543399f5bd6b5a
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-08 15:36:20 +0100

    add more information to the list of posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,15 @@
+{{-- @extends('layouts.navigation') --}}
+{{-- @section('content') --}}
 @forelse ($posts as $post)
-    <div>{{ $post->title }}</div>
+    <h2> Blog Posts </h2>
+    <hr />
+    <div class="blog-card">
+        <h3>{{ $post->title }}</h3>
+        <small> {{ $post->user->name }}</small>
+        <small>{{ $post->created_at }}</small>
+    </div>
+    <hr />
 @empty
     <div> There is no posts right now</div>
 @endforelse
+{{-- @endsection --}}
