0d26b6e211a0d698215f79f1063c93333b3db00f
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-10 00:20:48 +0100

    add tailwind styling

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.base')
 @section('content')
-    <div>
+    <div class="post">
         @if ($post)
-            <h1>{{ $post->title }}</h1>
-            <hr />
+            <h1 class="text-4xl font-bold">{{ $post->title }}</h1>
+            <hr class="h-0.5 w-full my-4 bg-gray-100 color-black" />
             <smaLl> <a href="/profile/{{ $post->user_id }}">{{ $post->user->name }}</a> -
                 {{ $post->created_at->format('j F, Y - H:i') }}</small>
-            <hr />
-            <p name="content" value={{ $post->content }}>{{ $post->content }}</p>
+            <hr class="h-0.5 w-full my-4 bg-gray-100 color-black" />
+            <p name="content" class="bg-white sm:p-8 sm:rounded-lg shadow py-full h-full w-full flex justify-center"
+                value={{ $post->content }}>
+                {{ $post->content }}</p>
     </div>
     </form>
 @else
