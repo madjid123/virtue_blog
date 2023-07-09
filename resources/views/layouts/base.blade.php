b27e82371c8b60e6f5a30dfca091c4471051a0c2
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-09 20:39:55 +0100

    create a base layout

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @if ($post)
+        <title>{{ $post->title }} </title>
+    @endif
+</head>
+
+<body>
+    <nav class="navbar">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        @auth
+            <a href="/profile">Profile</a>
+        @else
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+        @endauth
+        {{-- <a href="/toggle-dark-mode">Toggle Dark Mode</a> --}}
+    </nav>
+    @yield('content')
+</body>
+
+</html>
