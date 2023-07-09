b934dec9faf156bab91f5750c900a501da76a9a2
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-09 19:17:10 +0100

    add blog post view page

---
+++ resources/views/posts/view.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @if ($post)
+        <title>{{ $post->title }} </title>
+    @endif
+</head>
+
+<body>
+    <div>
+        @if ($post)
+            <h1>{{ $post->title }}</h1>
+            <hr />
+            <smaLl> <a href="/profile/{{ $post->user_id }}">{{ $post->user->name }}</a> - {{ $post->created_at }}</small>
+            <hr />
+            <p name="content" value={{ $post->content }}>{{ $post->content }}</p>
+    </div>
+    </form>
+@else
+    <h2> Nothing to display</h2>
+    @endif
+    </div>
+</body>
+
+</html>
