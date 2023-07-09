ca24b43251983909e6afd01b7e3be31d70eb6c2e
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-09 17:32:02 +0100

    add edit posts route

--- routes/posts.php
+++ routes/posts.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth")->group(function(){
 Route::get("/posts/create",PostController::class . "@create_form");
-Route::get("/posts/{id}/edit",PostController::class . "@edit_form");
+Route::get("/posts/{id}/edit",PostController::class . "@edit");
+Route::post("/posts/{id}/edit",PostController::class . "@edit_form");
 });
 Route::post("/posts/create",PostController::class . "@create");
 
