86c0f6d143d1f38e7a844d70b02e181cc9841160
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-09 16:40:20 +0100

    add edit form for a blog post.

--- routes/posts.php
+++ routes/posts.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth")->group(function(){
 Route::get("/posts/create",PostController::class . "@create_form");
+Route::get("/posts/{id}/edit",PostController::class . "@edit_form");
 });
 Route::post("/posts/create",PostController::class . "@create");
 
