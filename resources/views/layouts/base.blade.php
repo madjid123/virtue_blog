2beae74b18b249cc2258c6c7cb629b6e56e2c7b1
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-10 00:20:34 +0100

    add vite integration and some tailwind styling

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,12 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @vite(['resources/css/app.css', 'ressources/css/navbar.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     @if ($post)
         <title>{{ $post->title }} </title>
     @endif
 </head>
 
-<body>
+<body class="bg-gray-100">
     <nav class="navbar">
         <a href="/">Home</a>
         <a href="/about">About</a>
@@ -22,7 +25,9 @@
         @endauth
         {{-- <a href="/toggle-dark-mode">Toggle Dark Mode</a> --}}
     </nav>
-    @yield('content')
+    <div>
+        @yield('content')
+    </div>
 </body>
 
 </html>
