76831ad04fd224e003dea1e657b0a978f4cc26c1
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-09 20:27:13 +0100

    format date and add href to user profile

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -15,7 +15,8 @@
         @if ($post)
             <h1>{{ $post->title }}</h1>
             <hr />
-            <smaLl> <a href="/profile/{{ $post->user_id }}">{{ $post->user->name }}</a> - {{ $post->created_at }}</small>
+            <smaLl> <a href="/profile/{{ $post->user_id }}">{{ $post->user->name }}</a> -
+                {{ $post->created_at->format('j F, Y - H:i') }}</small>
             <hr />
             <p name="content" value={{ $post->content }}>{{ $post->content }}</p>
     </div>
