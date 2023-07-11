4dba02d5a6c0048d0db6aa1283ded25bd5f83197
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-11 18:25:57 +0100

    create za new script to toggle dark mode

---
+++ resources/js/ToggleDarkMode.js
@@ -0,0 +1,20 @@
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+
+// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+//     document.documentElement.classList.add('dark')
+// } else {
+//     document.documentElement.classList.remove('dark')
+// }
+function toggleDarkMode() {
+    const html = document.documentElement;
+    html.classList.toggle('dark');
+}
+// Whenever the user explicitly chooses light mode
+// function
+// localStorage.theme = 'light'
+
+// // Whenever the user explicitly chooses dark mode
+// localStorage.theme = 'dark'
+
+// // Whenever the user explicitly chooses to respect the OS preference
+// localStorage.removeItem('theme')
