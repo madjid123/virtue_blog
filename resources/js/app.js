b144cfbd5783ec80e1493cff34b30536dda182e8
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-11 18:25:42 +0100

    add toggleDarkMode script

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import "../css/navbar.css"
 import "../css/view_post.css"
+import './ToggleDarkMode'
 window.Alpine = Alpine;
 
 Alpine.start();
