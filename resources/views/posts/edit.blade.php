6f864ee90b42c021e22d26b3c59d27ed2ecfc436
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-12 18:15:05 +0100

    style edit form to similar to create post

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.base')
 @section('content')
-    <div>
+    <div class="flex flex-col py-4 items-center w-full h-full">
         @if ($post)
-            <h2>Edit post</h2>
-            <form action="/posts/{{ $post->id }}/edit" method="POST">
-                <div style="display:flex; flex-direction:column;">
+            <form action="/posts/{{ $post->id }}/edit" method="POST"
+                class="w-4/5 bg-white dark:bg-gray-800 shadow dark:text-white p-4 sm:rounded-lg h-4/5">
+                <h2 class="text-4xl font-bold p-3">Edit post</h2>
+                <div class="flex flex-col gap-5 w-full h-4/5 p-3  ">
                     @csrf
-                    <label>Title</label>
-                    <input placeholder="Enter the title of this blog post" name="title" value={{ $post->title }} />
+                    <label class="text-xl font-bold ">Title</label>
+                    <input placeholder="Enter the title of this blog post" name="title" value={{ $post->title }}
+                        class="sm:rounded-lg p-2 active:bg-blue-100 bg-gray-200 border border-black outline-blue-400 outline-1 dark:bg-gray-600" />
                     {{-- <label>Author</label>
             <input placeholder="Enter author's name" name="author" /> --}}
-                    <label>Content</label>
-                    <textarea placeholder="Enter the content of this post" name="content" value={{ $post->content }}>{{ $post->content }}</textarea>
-                    <input type="submit" />
+                    <label class="text-xl font-bold">Content</label>
+                    <textarea placeholder="Enter the content of this post" name="content" value={{ $post->content }}
+                        class=" sm:rounded-lg decoration-none bg-gray-200 dark:bg-gray-600 border border-black outline-blue-400 outline-1">{{ $post->content }}</textarea>
+                    <div class="flex justify-center items-center">
+                        <input type="submit" value="Edit"
+                            class="bg-green-400 dark:bg-green-500 sm:rounded-lg shadow p-3 w-1/5" />
+                    </div>
                 </div>
             </form>
         @else
