554d013e731a8a31b7fc8d0a3a1c31c21981dde5
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-12 18:45:53 +0100

    create about view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+@section('content')
+    <div class="flex flex-col items-center p-4 bg-white dark:bg-gray-800 sm:rounded-lg shadow m-2">
+        <h1 class="text-4xl dark:text-white font-bold dark:text-white p-3"> About</h1>
+        <p class="dark:text-white p-3">This app was created by Abdelmadjid Benimam for personal and learning purposes.
+            Moreover, Main features of this
+            app are to create, edit, delete and view blog post. The user must be authenticated to perform most of this
+            actions.
+            Finally this app was not intended for real world blog application hence it can be improved to reach that level.
+        </p>
+    </div>
+@endsection
