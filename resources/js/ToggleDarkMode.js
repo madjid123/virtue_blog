e4ff64e290ceb2886a2b5963ba0d253584126dda
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-13 18:38:41 +0100

    uncomment theme management

--- resources/js/ToggleDarkMode.js
+++ resources/js/ToggleDarkMode.js
@@ -1,10 +1,10 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 
-// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-//     document.documentElement.classList.add('dark')
-// } else {
-//     document.documentElement.classList.remove('dark')
-// }
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
 function toggleDarkMode() {
     const html = document.documentElement;
     html.classList.toggle('dark');
@@ -12,10 +12,10 @@ function toggleDarkMode() {
 document.getElementById('dark-mode').addEventListener('click', toggleDarkMode);
 // Whenever the user explicitly chooses light mode
 // function
-// localStorage.theme = 'light'
+localStorage.theme = 'light'
 
-// // Whenever the user explicitly chooses dark mode
-// localStorage.theme = 'dark'
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = 'dark'
 
-// // Whenever the user explicitly chooses to respect the OS preference
-// localStorage.removeItem('theme')
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem('theme')
