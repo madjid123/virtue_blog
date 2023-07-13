4330517b4da23b1dc33060303a70aae3aef906f7
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-13 23:08:04 +0100

    store display mode in localStorage

--- resources/js/ToggleDarkMode.js
+++ resources/js/ToggleDarkMode.js
@@ -1,21 +1,31 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+if (localStorage.getItem("dark") === 'true' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
 } else {
     document.documentElement.classList.remove('dark')
 }
 function toggleDarkMode() {
     const html = document.documentElement;
+    var isDarkMode = false;
     html.classList.toggle('dark');
+    isDarkMode = html.classList.contains("dark");
+    if (isDarkMode === true) {
+
+        localStorage.theme = 'dark';
+        localStorage.setItem("dark", "true")
+    } else {
+        localStorage.theme = 'light';
+        localStorage.setItem("dark", "false")
+    }
 }
-document.getElementById('dark-mode').addEventListener('click', toggleDarkMode);
+try {
+    document.getElementById('dark-mode').addEventListener('click', toggleDarkMode);
+} catch {
+    console.error("not element with id : dark-mode ")
+}
 // Whenever the user explicitly chooses light mode
 // function
-localStorage.theme = 'light'
-
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = 'dark'
 
 // Whenever the user explicitly chooses to respect the OS preference
-localStorage.removeItem('theme')
+// localStorage.removeItem('theme')
