5071640198f0e78d5286beaf41d92249592b39f8
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-18 13:40:43 +0100

    add edit post test case feature

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Ramsey\Collection\Map\AssociativeArrayMap;
 use Tests\TestCase;
 
@@ -12,11 +16,14 @@ class PostTest extends TestCase
     /**
      * A basic feature test example.
      */
+    use HasFactory;
     protected $sample_post = [
         "title"=> "test blog",
         "author" => "madjid",
         "content" => "this is a sample test."
     ];
+    protected $user ;
+    
     public function test_example(): void
     {
         $response = $this->post('/posts/create',$this->sample_post);
@@ -24,7 +31,14 @@ class PostTest extends TestCase
         $response->assertStatus(302);
     }
     public function test_edit_post():void 
-    {
-        $auth = $this->isAuthenticated();
+    {   
+        $user = User::factory(User::class)->make();
+        $post = Post::factory(Post::class)->make();
+        $this->actingAs($user)->post(`/posts/$post->id/create`,$post->toArray());
+        $post->content ="modfied";
+        $response = $this->actingAs($user)->post(`/posts/$post->id/edit`,$post->toArray());
+        
+        $response->assertStatus(302);
+        
     }
 }
