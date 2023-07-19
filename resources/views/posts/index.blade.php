4d15789ea4d4aa360000b8f5fdd34b2c61db1028
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-19 23:43:24 +0100

    fix post card layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,30 +4,34 @@
         <h2 class="text-4xl font-bold pb-4 dark:text-white"> Blog Posts </h2>
         <div class="flex items-start flex-col w-4/5">
             @forelse ($posts as $post)
-                <div class="blog-card p-4 bg-white dark:bg-gray-800 dark:text-white sm:rounded-lg shadow w-full my-1">
-                    <h3 class="text-2xl font-bold"> <a href="/posts/{{ $post->id }}/view"
-                            class="hover:text-blue-400">{{ $post->title }}</a></h3>
-                    <small> <a href="/profile/{{ $post->user_id }}"
-                            class="hover:text-red-400 dark:text-gray-200 text-gray-700">{{ $post->user->name }}</a> -
-                    </small>
-                    <small>{{ $post->created_at->format('j F, Y ') }}</small>
+                <div
+                    class="blog-card p-4 bg-white dark:bg-gray-800 dark:text-white sm:rounded-lg shadow w-full my-1 flex flex-row justify-between">
+                    <div>
+                        <h3 class="text-2xl font-bold"> <a href="/posts/{{ $post->id }}/view"
+                                class="hover:text-blue-400">{{ $post->title }}</a></h3>
+                        <small> <a href="/profile/{{ $post->user_id }}"
+                                class="hover:text-red-400 dark:text-gray-200 text-gray-700">{{ $post->user->name }}</a> -
+                        </small>
+                        <small>{{ $post->created_at->format('j F, Y ') }}</small>
+                    </div>
                     @auth
-
-                        @if (Auth::user()->id == $post->user->id)
-                            <form action="/posts/{{ $post->id }}/delete" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <input class="text-white  bg-red-600 dark:bg-red-400 p-1 rounded-lg px-2 text-sm" type=submit
-                                    value="Delete">
-                            </form>
-                            <button class="text-white bg-yellow-400 dark:bg-yellow-400 p-1 rounded-lg px-2 text-sm"
-                                type="submit"><a href="/posts/{{ $post->id }}/edit">Edit</a></button>
-                        @endif
+                        <div class="flex flex-row  gap-2 justify-center p-3">
+                            @if (Auth::user()->id == $post->user->id)
+                                <form action="/posts/{{ $post->id }}/delete" method="post" class="flex flex-row">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input class="text-white  bg-red-600 dark:bg-red-400 p-1 rounded-lg px-2 text-sm"
+                                        type=submit value="Delete">
+                                </form>
+                                <button class="text-white bg-yellow-400 dark:bg-yellow-400 p-1 rounded-lg px-2 text-sm"
+                                    type="submit"><a href="/posts/{{ $post->id }}/edit">Edit</a></button>
+                            @endif
+                        </div>
                     @endauth
                 </div>
 
             @empty
-                <div> There is no posts right now</div>
+                <div class="text-gray-600 dark:text-white"> There is no posts right now</div>
             @endforelse
         </div>
     </div>
