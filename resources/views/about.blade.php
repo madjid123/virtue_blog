77a107aa32dd16fad06ab18e29c3b01c2d143d0c
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-07-20 18:56:20 +0100

    decrease width and height of about card

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.base')
 @section('content')
-    <div class="flex flex-col items-center p-4 bg-white dark:bg-gray-800 sm:rounded-lg shadow m-2">
+    <div class="h-5/6 w-4/5 flex flex-col items-center p-6 bg-white dark:bg-gray-800 sm:rounded-lg shadow m-4">
         <h1 class="text-4xl dark:text-white font-bold dark:text-white p-3"> About</h1>
         <p class="dark:text-white p-3">This app was created by Abdelmadjid Benimam for personal and learning purposes.
             Moreover, Main features of this
